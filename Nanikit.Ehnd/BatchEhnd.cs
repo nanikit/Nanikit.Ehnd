4367b48e571fd26430fbbc9f8242481ce6dbbba9
nanikit <nanikit4@gmail.com>
2024-01-07 12:29:56 +0900

    fix: substring logic error when split

--- Nanikit.Ehnd/BatchEhnd.cs
+++ Nanikit.Ehnd/BatchEhnd.cs
@@ -11,15 +11,31 @@ namespace Nanikit.Ehnd {
   /// It merge / split translation works for single threaded deterministic translator.
   /// </summary>
   public class BatchEhnd : IEhnd, IDisposable {
+    private readonly CancellationTokenSource _cancellation = new();
 
-    private static IEnumerable<string> SplitBySegmentNewline(string merged, IEnumerable<string> texts) {
-      int startIdx = 0;
-      foreach (string text in texts) {
-        int newlineCount = text.Count((c) => c == '\n');
-        int endIdx = GetNthNewlineFrom(merged, newlineCount, startIdx);
-        yield return merged.Substring(startIdx, endIdx);
-        startIdx = endIdx + 1;
-      }
+    private readonly IEhnd _ehnd;
+
+    private readonly Channel<Work> _works = Channel.CreateUnbounded<Work>(new UnboundedChannelOptions() {
+      SingleReader = true,
+      SingleWriter = true,
+      AllowSynchronousContinuations = true,
+    });
+
+    public BatchEhnd(IEhnd ehnd) {
+      _ehnd = ehnd;
+      _ = ProcessQueue();
+    }
+
+    public void Dispose() {
+      _cancellation.Cancel();
+      _cancellation.Dispose();
+      GC.SuppressFinalize(this);
+    }
+
+    public async Task<string> TranslateAsync(string japanese, CancellationToken? cancellationToken = null) {
+      var work = new Work(japanese);
+      await _works.Writer.WriteAsync(work).ConfigureAwait(false);
+      return await work.Client.Task;
     }
 
     private static int GetNthNewlineFrom(string merged, int newlineCount, int startIdx) {
@@ -35,48 +51,36 @@ namespace Nanikit.Ehnd {
       return idx;
     }
 
-    public BatchEhnd(IEhnd ehnd) {
-      _ehnd = ehnd;
-      _worker = ProcessQueue();
-    }
-
-    public async Task<string> TranslateAsync(string japanese, CancellationToken? cancellationToken = null) {
-      var work = new Work(japanese);
-      await _works.Writer.WriteAsync(work).ConfigureAwait(false);
-      return await work.Client.Task;
-    }
-
-    public void Dispose() {
-      _cancellation.Cancel();
-      _cancellation.Dispose();
-      GC.SuppressFinalize(this);
-    }
-
-    private class Work {
-      public readonly string Text;
-      public readonly TaskCompletionSource<string> Client;
-
-      public Work(string text) {
-        Text = text;
-        Client = new TaskCompletionSource<string>();
+    private static IEnumerable<string> SplitBySegmentNewline(string merged, IEnumerable<string> texts) {
+      int startIdx = 0;
+      foreach (string text in texts) {
+        int newlineCount = text.Count((c) => c == '\n');
+        int endIdx = GetNthNewlineFrom(merged, newlineCount, startIdx);
+        yield return merged.Substring(startIdx, endIdx - startIdx);
+        startIdx = endIdx + 1;
       }
     }
 
-    private readonly IEhnd _ehnd;
-    private readonly Task _worker;
-    private readonly Channel<Work> _works = Channel.CreateUnbounded<Work>(new UnboundedChannelOptions() {
-      SingleReader = true,
-      SingleWriter = true,
-      AllowSynchronousContinuations = true,
-    });
-    private readonly CancellationTokenSource _cancellation = new();
+    private async Task<List<Work>> GetWorksOfThisRound(CancellationToken token) {
+      var works = new List<Work>();
+      long length = 0;
+      var reader = _works.Reader;
+
+      await reader.WaitToReadAsync(token).ConfigureAwait(false);
+      while (length < 4000 && reader.TryRead(out var work)) {
+        length += work.Text.Length;
+        works.Add(work);
+      }
+
+      return works;
+    }
 
     private async Task ProcessQueue() {
-      CancellationToken token = _cancellation.Token;
+      var token = _cancellation.Token;
       while (!token.IsCancellationRequested) {
-        List<Work> works = await GetWorksOfThisRound(token).ConfigureAwait(false);
+        var works = await GetWorksOfThisRound(token).ConfigureAwait(false);
 
-        IEnumerable<string> texts = works.Select(x => x.Text);
+        var texts = works.Select(x => x.Text);
         string mergedStart = string.Join("\n", texts);
 
         try {
@@ -96,18 +100,14 @@ namespace Nanikit.Ehnd {
       }
     }
 
-    private async Task<List<Work>> GetWorksOfThisRound(CancellationToken token) {
-      var works = new List<Work>();
-      long length = 0;
-      var reader = _works.Reader;
+    private class Work {
+      public readonly TaskCompletionSource<string> Client;
+      public readonly string Text;
 
-      await reader.WaitToReadAsync(token).ConfigureAwait(false);
-      while (length < 4000 && reader.TryRead(out Work? work)) {
-        length += work.Text.Length;
-        works.Add(work);
+      public Work(string text) {
+        Text = text;
+        Client = new TaskCompletionSource<string>();
       }
-
-      return works;
     }
   }
 }
